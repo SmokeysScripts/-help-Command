030f2a252184d6e72208b04a8f51f30aba1d0486
SmokeysScripts <Ianmarkealli14@gmail.com>
2025-03-01 13:35:41 -0500

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+author 'ian_b_creamin on discord'
+description 'simple help command'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
